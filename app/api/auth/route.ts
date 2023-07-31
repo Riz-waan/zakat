198f20294d558952d6cf532966ea10d5babd10d8
Riz-waan <rizraak@hotmail.com>
2023-07-30 22:26:53 -0400

    added jwt

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,7 @@
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
-
+import * as jose from "jose"
+import { authType } from '@/app/(utils)/types/home';
 export function GET(request: NextRequest) {
     return NextResponse.json(
         {
@@ -18,12 +19,13 @@ export function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
     try {
         const body = await request.json()
-
         if (body.username === "user" && body.password === "password") {
+            const generatedTokens = await generateTokens(body.username)
+
             return NextResponse.json(
                 {
-                    accessToken: "coolAccessToken",
-                    refreshToken: "coolRefreshToken"
+                    accessToken: generatedTokens.accessToken,
+                    refreshToken: generatedTokens.refreshToken
                 }
             )
         }
@@ -33,20 +35,62 @@ export async function POST(request: NextRequest) {
     }
 }
 
+
+const jwtOptions = {
+    secret: new TextEncoder().encode('cc7e0d44fd473002f1c42167459001140ec6389b7353f8088f4d9a95f2f596f2'),
+    alg: 'HS256',
+    issuer: 'rizco:zakat',
+    audience: 'rizco:zakat',
+    accessTokenExpiration: "2h",
+    refreshTokenExpiration: "30s"
+}
+
 export async function PATCH(request: NextRequest) {
     try {
         const body = await request.json()
-        if (body.refreshToken === "coolRefreshToken") {
+        const {payload } = await jose.jwtVerify(body.refreshToken, jwtOptions.secret, {
+            issuer: jwtOptions.issuer,
+            audience: jwtOptions.audience
+        })
+
+        if (payload.type === "refreshToken") {
+
+            const generatedTokens = await generateTokens(payload.username as string)
+
             return NextResponse.json(
                 {
-                    accessToken: "coolAccessToken",
-                    refreshToken: "coolRefreshToken"
+                    accessToken: generatedTokens.accessToken,
+                    refreshToken: generatedTokens.refreshToken
                 }
-            )
+                )
 
         }
         throw new Error("Unexpected User");
     } catch (e) {
         return NextResponse.json({accessToken: "", refreshToken: ""}, {status: 403})
     }
+}
+
+
+
+
+const generateTokens = async (username: string): Promise<authType> => {
+
+    const accessToken = await new jose.SignJWT({ username, type: "accessToken" })
+  .setProtectedHeader({ alg: jwtOptions.alg })
+  .setIssuedAt()
+  .setIssuer(jwtOptions.issuer)
+  .setAudience(jwtOptions.audience)
+  .setExpirationTime(jwtOptions.accessTokenExpiration)
+  .sign(jwtOptions.secret)
+
+    const refreshToken = await new jose.SignJWT({ username, type: "refreshToken" })
+  .setProtectedHeader({ alg: jwtOptions.alg })
+  .setIssuedAt()
+  .setIssuer(jwtOptions.issuer)
+  .setAudience(jwtOptions.audience)
+  .setExpirationTime(jwtOptions.refreshTokenExpiration)
+  .sign(jwtOptions.secret)
+
+    return {accessToken, refreshToken}
 }
